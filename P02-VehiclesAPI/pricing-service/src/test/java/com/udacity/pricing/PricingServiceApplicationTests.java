df598e8dec825c0f47f30e6d9f7369269e666aeb
Krupesh Anadkat <kmanadkat@gmail.com>
2024-04-10 12:56:13 +0530

    feat: Configured Pricing Service As Eureka Client & Added UT

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,41 @@
 package com.udacity.pricing;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import com.udacity.pricing.api.PricingController;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.http.MediaType;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(PricingController.class)
 public class PricingServiceApplicationTests {
 
-	@Test
+	@Autowired
+	private MockMvc mockMvc;
+
 	public void contextLoads() {
+	}
+
+	@Test
+	public void getPriceByVehicleId() throws Exception {
+		int vehicleId = 2;
+		mockMvc.perform(get("/services/price?vehicleId=" + vehicleId))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void getPriceByInvalidVehicleId() throws Exception {
+		int vehicleId = 21;
+		mockMvc.perform(get("/services/price?vehicleId=" + vehicleId))
+				.andExpect(status().isNotFound());
 	}
 
 }
